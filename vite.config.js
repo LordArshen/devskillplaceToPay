3e330f5b3bd8cd7fc33dcb41116d60af8b1bc249
LordArshen <m.r.barrera@hotmail.com>
2022-06-01 15:27:19 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: '@', replacement: path.resolve(__dirname, './src'),
-      },
-      {
-        find: './runtimeConfig', replacement: './runtimeConfig.browser',
-      }
-
-    ]
-  }
-}
-,)
+  plugins: [react()]
+})
